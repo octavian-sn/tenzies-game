16e2bb7194be792c491b147e232843ff39d2dc5e
Sulic Octavian <octavius1992@gmail.com>
2023-01-31 13:05:27 +0200

    Add die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import Die from './components/Die';
 
 function App() {
   return (
     <div id="project">
-      <main>yes</main>;
+      <main>
+        <div className="dice-container">
+          <Die value="1" />
+          <Die value="2" />
+          <Die value="3" />
+          <Die value="4" />
+          <Die value="5" />
+          <Die value="6" />
+          <Die value="7" />
+          <Die value="8" />
+          <Die value="9" />
+          <Die value="10" />
+        </div>
+      </main>
+      ;
     </div>
   );
 }
